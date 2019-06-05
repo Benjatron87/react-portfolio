a166eeac5cb6c2e34577ff49162d5f2760dddff6
Benjatron <benjt122@gmail.com>
2019-06-05 12:07:44 -0400

    About and Portfolio pages are essentially done

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -11,8 +11,14 @@ class Home extends Component {
           </div>
 
             <div className="col-10 summary">
-            Hello, my name is Benjamin Thomsen (Benj for short). I was born in Raleigh, North Carolina and have lived here my whole life. I graduated from Appalachian State University with a degree in Sustainable Technology and a Minor in Physics in May of 2018. I have also graduated from the UNC Chapel Hill coding bootcamp where I learned how to make this webpage and much more. In my personal life, I enjoy backpacking, playing basketball, and playing ultimate frisbee.
-            I played five years of college ultimate, and have played four years of summer elite club with a local team called Turbine.I have also been on the practice squad for two professional ultimate teams: The Charlotte Express, and The Raleigh Flyers. I am currently looking for work doing some kind of software development. I have become very familiar with javascript, html and css as well as databases such as Google Firebase, MySql and MongoDB. I know node.js as well as React and understand the fundamentals of both front-end and back-end technologies and how to deploy them properly.
+
+              <p>
+                Hello, my name is Benjamin Thomsen (Benj for short). I was born in Raleigh, North Carolina and have lived in the area my whole life. I graduated from Appalachian State University with a degree in Sustainable Technology and a Minor in Physics in May of 2018. I have also graduated from the UNC Chapel Hill coding bootcamp where I learned how to make this webpage and much more. In my personal life, I enjoy backpacking, playing basketball, and playing ultimate frisbee.
+                I played five years of college ultimate, and have played four years of summer elite club with a local club team called Turbine.I have also been on the practice squad for two professional ultimate teams: The Charlotte Express, and The Raleigh Flyers. 
+              </p>
+              <p>
+                I am currently looking for work doing software development. I have become very familiar with Javascript/Jquery, HTML and CSS as well as databases such as  MySql and MongoDB. I know Node.js as well as React and understand the fundamentals of both front-end and back-end technologies and how to deploy them properly. I have also delved into Arduino and combining my love of hardware with software to create a handful of fun projects.
+              </p>
             </div>
         </Wrapper>
       </div>
