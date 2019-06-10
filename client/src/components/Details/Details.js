ddf2b26cd091c83c478c19f49ecb8edf5981be90
Benjatron <benjt122@gmail.com>
2019-06-10 13:51:03 -0400

    Finished the details pages. Added an open icon so that users can go to some of my deployed projects.

--- client/src/components/Details/Details.js
+++ client/src/components/Details/Details.js
@@ -13,9 +13,25 @@ class Details extends Component {
             <Wrapper title={this.state.project.name}>
                 <div className="row">
                     <div>
+
                         <img src={this.state.project.image}  alt="oops" className="details-image"></img>
-                        <a className="git-link" href={this.state.project.link} target="blank">GitHub Link</a>
+
+                        {this.state.project.deployed ? 
+                        
+                            <a className="deployed-link" href={this.state.project.deployed} target="blank">
+
+                                <img src="/images/open-icon.png" height="32px" width="32px" alt="open"></img>
+                            </a>
+                        
+                        : ""}
+
+                        <a className="git-link" href={this.state.project.link} target="blank">
+
+                            <img src="/images/GitHub-Mark-32px.png" alt="github"></img>
+                        </a>
+
                         <br></br>
+
                         <p className="details-summary">
                             {this.state.project.summary}
                         </p>
