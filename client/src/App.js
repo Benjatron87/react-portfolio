96b5b2b53876f470f96f204968854a6a59598df9
Benjatron <benjt122@gmail.com>
2019-06-20 09:18:58 -0400

    small change

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as HashRouter, Route } from "react-router-dom";
+import { HashRouter, Route } from "react-router-dom";
 import Portfolio from "./pages/Portfolio";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
